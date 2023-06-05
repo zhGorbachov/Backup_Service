a493ab7098d64fe4b7d25de3878278d16b8a50d2
Evgeniy Gorbachov <zekagor@gmail.com>
2023-06-05 14:15:01 +0300

    Was added authorization argument to methods, which need to be authorized.

--- Backup_Service/PresentationLayer/Controllers/HomeController.cs
+++ Backup_Service/PresentationLayer/Controllers/HomeController.cs
@@ -1,7 +1,11 @@
 ﻿using System.Diagnostics;
+using AutoMapper;
+using BusinessLayer.Interfaces;
+using BusinessLayer.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.StaticFiles;
+using PresentationLayer.DTO;
 using PresentationLayer.Models;
 
 namespace PresentationLayer.Controllers;
@@ -10,12 +14,16 @@ public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
     private static string _path { get; set; }
+    private readonly IBackupService _backupService;
+    private readonly IMapper _mapper;
     private readonly IWebHostEnvironment _hostEnvironment;
 
-    public HomeController(ILogger<HomeController> logger, IWebHostEnvironment webHostEnvironment)
+    public HomeController(ILogger<HomeController> logger, IWebHostEnvironment webHostEnvironment, IMapper mapper, IBackupService backupService)
     {
         _logger = logger;
         _hostEnvironment = webHostEnvironment;
+        _backupService = backupService;
+        _mapper = mapper;
     }
     
     public async Task<IActionResult> Index()
@@ -23,45 +31,51 @@ public class HomeController : Controller
         return View();
     }
     
-    
+    [Authorize]
     public async Task<IActionResult> UploadingPage()
     {
         return View();
     }
     
+    [Authorize]
     public async Task<IActionResult> DownloadingFilePage()
     {
-        Console.WriteLine("FFAAA");
-        Console.WriteLine(_path);
         return View();
     }
     
-    // [Authorize]
-    [HttpPost("UploadFiles")]
-    public async Task<IActionResult> UploadFile(List<IFormFile> files)
+    [HttpPost("UploadFiles"), Authorize]
+    public async Task UploadFile(List<IFormFile> files)
     {
         Console.WriteLine(files.Count());
         if (files.Count > 0 && files != null)
         {
-            Console.WriteLine("ASDASD");
+            var backupDTO = new BackupDTO
+            {
+                Name = "Backup",
+                IdStorage = 1,
+                TariffType = 1,
+                CreationTime = DateTime.Now,
+                Size = 0,
+                IdAccount = 5
+            };
             var fileB = new FilePath()
             {
-                filePath = await FileWorking.UploadFile(files, "zhekagor", 1),
+                filePath = await FileWorking.UploadFile(files, backupDTO),
             };
-            Console.WriteLine(fileB.filePath);
+
+            backupDTO.Size = new FileInfo(fileB.filePath).Length;
+            var backupModel = _mapper.Map<BackupModel>(backupDTO);
+            await _backupService.AddBackupAsync(backupModel);
             _path = fileB.filePath;
-            return RedirectToAction("DownloadingFilePage", "Home");
+            Console.WriteLine(_path);
         }
-        Console.WriteLine("GG");
-        return Redirect("/Home");
     }
 
     [HttpGet("GetFile")]
     public async Task<IActionResult> DownloadFile()
     {
-        Console.WriteLine(333);
+        Console.WriteLine(_path);
         var filePath = Path.Combine(_hostEnvironment.WebRootPath, _path);
-        Console.WriteLine(222);
         var fileName = Path.GetFileName(filePath);
 
         if (System.IO.File.Exists(filePath))
